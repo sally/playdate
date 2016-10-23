4dbe973dd3904c597970dc9164f6d4aac9268e7a
Young Ju Park <parkyngj@gmail.com>
2016-10-22 22:38:03 -0700

    Attempt killing arrow upon mouseleave

--- app/assets/javascripts/profile_page.js
+++ app/assets/javascripts/profile_page.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     var $btnSets = $('#responsive'),
     $btnLinks = $btnSets.find('a');
  
-    $btnLinks.click(function(e) {
+    $btnLinks.hover(function(e) {
         e.preventDefault();
         $(this).siblings('a.active').removeClass("active");
         $(this).addClass("active");
@@ -10,6 +10,12 @@ $(document).ready(function() {
         $(".map-container").addClass('hidden');
         $("div.user-menu>div.user-menu-content").removeClass("active");
         $("div.user-menu>div.user-menu-content").eq(index).addClass("active");
+    });
+
+    $btnLinks.mouseleave(function(e){
+        $('div.btn-group-vertical>a').css('border-right', '0px solid transparent');
+        $(".user-menu-content").removeClass('active');
+        $(".map-container").removeClass('hidden');
     });
 });
 
