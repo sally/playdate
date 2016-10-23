b878c89e9fe2a0ac13108c764085f8cd37abe1bd
Sam Leiken <sam.k.leiken@gmail.com>
2016-10-22 18:58:57 -0700

    adds chat box to chat view and some basic chatt css

--- app/assets/javascripts/chat.js
+++ app/assets/javascripts/chat.js
@@ -1,1 +1,32 @@
-alert("HEY CHAT")
+var config = {
+  apiKey: "AIzaSyDw6D_RWJbfJsWuxCyf_8tbvt4fUAZhZhI",
+  authDomain: "dayshare-9e38a.firebaseapp.com",
+  databaseURL: "https://dayshare-9e38a.firebaseio.com"
+};
+firebase.initializeApp(config);
+
+function login() {
+  // Log the user in via Twitter
+  var provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(provider).catch(function(error) {
+    console.log("Error authenticating user:", error);
+  });
+}
+
+firebase.auth().onAuthStateChanged(function(user) {
+  // Once authenticated, instantiate Firechat with the logged in user
+  if (user) {
+    initChat(user);
+  }
+});
+
+function initChat(user) {
+  // Get a Firebase Database ref
+  var chatRef = firebase.database().ref("chat");
+
+  // Create a Firechat instance
+  var chat = new FirechatUI(chatRef, document.getElementById("firechat-wrapper"));
+
+  // Set the Firechat user
+  chat.setUser(user.uid, user.displayName);
+}
